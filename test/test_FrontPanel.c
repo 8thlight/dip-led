dfcd78fe9fffde49bac83300db670a26ec5088df
dougbradbury <doug@8thlight.com>
2014-10-27 13:11:26 -0500

    test front panel

--- test/test_FrontPanel.c
+++ test/test_FrontPanel.c
@@ -1,30 +1,53 @@
 #include "unity.h"
 #include "FrontPanel.h"
 
+static uint16_t lastOffLed = 0xFFFF;
 void TestLedOff(uint16_t pin)
 {
+  lastOffLed = pin;
 }
 
+static uint16_t lastOnLed = 0xFFFF;
 void TestLedOn(uint16_t pin)
 {
+  lastOnLed = pin;
 }
 
+static uint16_t lastCheckedButton = 0xFFFF;
+static int theButtonIsPressed = 0;
 int TestButtonIsPresed(uint16_t pin)
 {
-  return 0;
+  lastCheckedButton = pin;
+  return theButtonIsPressed;
 }
 
 
 void setUp(void)
 {
   FrontPanel_Create(&TestButtonIsPresed, &TestLedOn, &TestLedOff);
+  lastCheckedButton = 0xFFFF;
+  lastOnLed = 0xFFFF;
+  lastOffLed = 0xFFFF;
 }
 
 void tearDown(void)
 {
 }
 
-void test_module_generator_needs_to_be_implemented(void)
+void test_led_is_turned_on(void)
 {
+  theButtonIsPressed = 1;
   FrontPanel_Update();
+  TEST_ASSERT_EQUAL(lastCheckedButton, 3);
+  TEST_ASSERT_EQUAL(lastOnLed, STATUS_LED);
+  TEST_ASSERT_EQUAL(lastOffLed, 0xFFFF);
+}
+
+void test_led_is_turned_off(void)
+{
+  theButtonIsPressed = 0;
+  FrontPanel_Update();
+  TEST_ASSERT_EQUAL(lastCheckedButton, 3);
+  TEST_ASSERT_EQUAL(lastOnLed, 0xFFFF);
+  TEST_ASSERT_EQUAL(lastOffLed, STATUS_LED);
 }
